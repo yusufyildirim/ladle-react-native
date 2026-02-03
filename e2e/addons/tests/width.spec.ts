0cba9023ccfcc44e1bc7920c76ac107a9f08740b
Yusuf YILDIRIM <yusuf@tinythin.gs>
2026-02-03 10:52:01 +0100

    Revert "fix: use toHaveCSS for auto-waiting in width test"
    This reverts commit f5c523d7704d4f5c3eb344344d0e3a318c2cc0a5.

--- e2e/addons/tests/width.spec.ts
+++ e2e/addons/tests/width.spec.ts
@@ -41,7 +41,10 @@ test("custom width is used and added to the addon popup, apply stylesheet", asyn
   await page.waitForSelector("#width-custom");
   const iframe = page.frameLocator("iframe");
   const h1 = iframe.locator("h1");
-  await expect(h1).toHaveCSS("color", "rgb(255, 192, 203)");
+  const color = await h1.evaluate((element) =>
+    window.getComputedStyle(element).getPropertyValue("color"),
+  );
+  expect(color).toBe("rgb(255, 192, 203)");
 });
 
 test("allow key as a value for width", async ({ page }) => {
